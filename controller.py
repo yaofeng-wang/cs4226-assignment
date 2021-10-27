3608859e7cc8a30d67a202d674130551c6197cc1
yaofeng-wang <yaofeng96@gmail.com>
2021-10-27 19:10:07 +0800

    Refactor controller

--- controller.py
+++ controller.py
@@ -19,11 +19,19 @@ from pox.lib.util import dpid_to_str
 from pox.lib.addresses import IPAddr, EthAddr
 
 log = core.getLogger()
+LOG_UNKOWN_POLICY = "Unknown policy"
+LOG_PACKET_ARRIVED_FMT = "%d:%s arrived"
+LOG_INSTALLING_FLOW_FMT = "%d:Installing flow for %s"
+LOG_FLOOD_WITH_EXPIRED_ENTRY_FMT = "%d:Flood with expired entry"
+LOG_FLOOD_FMT = "%d:Flood"
+LOG_SWITCH_CAME_UP_FMT = "Switch %s has come up."
 
-TTL = 65535 # in seconds
+TTL = 20 # in seconds
 FIREWALL_PRIORITY = 100
 PREMIUM_TRAFFIC_PRIORITY = 10
 LEARNING_SWITCH_PRIORITY = 1
+POLICY_FILE_NAME = "policy.in"
+READ_MODE = "r"
 
 def ip_to_mac(ip):
     b = ["0" + hex(int(v)).split("x")[-1] if len(hex(int(v)).split("x")[-1]) == 1 else hex(int(v)).split("x")[-1] for v in ip.split(".")]
@@ -35,51 +43,49 @@ class Controller(EventMixin):
         core.openflow_discovery.addListeners(self)
         self.storage = dict()
         self.ttl = TTL
+        self.num_firewall_policies = 0
+        self.num_premium_addr = 0
+        self.policies = []
+        self.premium_addr = []
+        self.read_policies()
 
-        self.premium_addr = [
-            ip_to_mac("10.0.0.1"),
-            ip_to_mac("10.0.0.3"),
-            ip_to_mac("10.0.0.7")
-        ]
+    def read_policies(self):
+        with open(POLICY_FILE_NAME, READ_MODE) as infile:
+            for i, line in enumerate(infile):
+                if i == 0:
+                    self.num_firewall_policies, self.num_premium_addr = [int(v) for v in line.split()]
+                elif i <= self.num_firewall_policies:
+                    l = line.split(',')
+                    l[-1] = int(l[-1].strip())
+                    self.policies.append(l)
+                else:
+                    self.premium_addr.append(ip_to_mac(line))
 
-        self.policies = [
-            ["10.0.0.1", 4001],
-            ["10.0.0.1", "10.0.0.2", 1000],
-        ]
-
-        print "Firewall rules:"
+        print("%d, %d" % (self.num_firewall_policies, self.num_premium_addr))
+        print("Firewall rules:")
 
         for policy in self.policies:
             if len(policy) == 2:
-                print "{'src': '%s', 'port':'%d\\n'}\n" % \
-                    (policy[0], policy[1])
+                print("{'src': '%s', 'port':'%d\\n'}\n" % (policy[0], policy[1]))
             elif len(policy) == 3:
-                print "{'src': '%s', 'dst': '%s', 'port':'%d\\n'}\n" % \
-                    (policy[0], policy[1], policy[2])
+                print("{'src': '%s', 'dst': '%s', 'port':'%d\\n'}\n" % (policy[0], policy[1], policy[2]))
 
-        print "Premium connections:"
+        print("Premium connections:")
         for conn in self.premium_addr:
-            print "%s\n" % conn
+            print("%s\n" % conn)
+
 
     def resend_packet(self, dpid, packet_in, out_port, qid):
-        """
-        Instructs the switch to resend a packet that it had sent to us.
-        "packet_in" is the ofp_packet_in object the switch had sent to the
-        controller due to a table-miss.
-        """
         msg = of.ofp_packet_out()
         msg.data = packet_in
-        # action = of.ofp_action_enqueue(port = out_port, queue_id=qid)
-        # msg.actions.append(action)
         action = of.ofp_action_output(port = out_port)
         msg.actions.append(action)
         core.openflow.sendToDPID(dpid, msg)
 
     def _handle_PacketIn(self, event):
         packet = event.parsed
-        log.debug("%d:%s arrived" % (event.dpid, packet))
-
         dpid = event.dpid
+        log.debug(LOG_PACKET_ARRIVED_FMT % (dpid, packet))
         packet_in = event.ofp
         dst = packet.dst
         src = packet.src
@@ -92,7 +98,7 @@ class Controller(EventMixin):
         mac_to_port[src] = (packet_in.in_port, current_time)
 
         if dst in mac_to_port and current_time - mac_to_port[dst][1] <= self.ttl:
-            log.debug("%d:Installing flow for %s" % (event.dpid, packet))
+            log.debug(LOG_INSTALLING_FLOW_FMT % (dpid, packet))
 
             dst_port = mac_to_port[dst][0]
             self.resend_packet(dpid, packet_in, dst_port, qid)
@@ -100,31 +106,23 @@ class Controller(EventMixin):
             msg = of.ofp_flow_mod()
             msg.priority = priority
             msg.hard_timeout = self.ttl
-            msg.match = of.ofp_match() # redundant
             msg.match.dl_dst = dst
             msg.match.dl_src = src
             action = of.ofp_action_enqueue(port=dst_port, queue_id=qid)
             msg.actions.append(action)
-            # action = of.ofp_action_output(port = dst_port)
-            # msg.actions.append(action)
             core.openflow.sendToDPID(dpid, msg)
         elif dst in mac_to_port:
-            log.debug("%d:Flood with expired entry" % event.pid)
+            log.debug(LOG_FLOOD_WITH_EXPIRED_ENTRY_FMT % dpid)
             mac_to_port.pop(dst)
             self.resend_packet(dpid, packet_in, of.OFPP_FLOOD, qid)
         else:
-            log.debug("%d:Flood" % event.dpid)
+            log.debug(LOG_FLOOD_FMT % dpid)
             self.resend_packet(dpid, packet_in, of.OFPP_FLOOD, qid)
-
-        # for k, v in self.storage.items():
-        #     log.debug("%d switch %s: %s...(%s)" % (event.dpid, k, v.keys(), time.time()))
-        # log.debug("\n")
 
     def _handle_ConnectionUp(self, event):
         dpid = dpid_to_str(event.dpid)
-        log.debug("Switch %s has come up.", dpid)
+        log.debug(LOG_SWITCH_CAME_UP_FMT % dpid)
 
-        # Send the firewall policies to the switch
         def sendFirewallPolicy(connection, policy):
             msg = of.ofp_flow_mod()
             msg.priority = FIREWALL_PRIORITY
@@ -146,18 +144,13 @@ class Controller(EventMixin):
                 msg.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
                 connection.send(msg)
             else:
-                log.debug("Unknown policy")
+                log.debug(LOG_UNKOWN_POLICY)
 
         for policy in self.policies:
             sendFirewallPolicy(event.connection, policy)
-        # print(dir(event.connection))
-        # eth_addr = event.connection.eth_addr
-        # self.premium_addr = [eth_addr if addr == eth_addr else addr for addr in self.premium_addr]
 
 def launch():
-    # Run discovery and spanning tree modules
     pox.openflow.discovery.launch()
     pox.openflow.spanning_forest.launch()
 
-    # Starting the controller module
     core.registerNew(Controller)
